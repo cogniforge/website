69558ec615162463d37b2191f7ebcdc6af174ede
Zeyu Yao <novodoodle@gmail.com>
2023-05-04 22:48:40 +0800

    Refactor logo as a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,9 @@
-import Image from "next/image";
-import Carousel from "@/components/Carousel";
 import Link from 'next/link';
 
 import { photosLeft, photosRight } from "@/data/photos";
 
-const Logo = () => {
-    return (
-        <div className="flex justify-between items-center mt-12 py-6 md:py-12">
-            <div className="flex items-center">
-                <Image
-                    className="mr-4 dark:filter dark:invert"
-                    src="/logo.svg"
-                    alt="Logo"
-                    width={60}
-                    height={60}
-                />
-                <h1 className="text-3xl md:text-4xl">cogniforge</h1>
-            </div>
-        </div>
-    );
-};
+import Carousel from "@/components/Carousel";
+import Logo from "@/components/Logo";
 
 export default function Home() {
     return (
