6d17bcbabfa62c5b1bc7c95f3ceb72de85802e91
Zeyu Yao <novodoodle@gmail.com>
2023-05-04 23:13:17 +0800

    Format .tsx files

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return (
-        <div className="flex justify-between items-center mt-12 py-6 md:py-12">
-            <div className="flex items-center">
-                <Image
-                    className="mr-4 dark:filter dark:invert"
-                    src="/logo.svg"
-                    alt="Logo"
-                    width={60}
-                    height={60}
-                />
-                <h1 className="text-3xl md:text-4xl">cogniforge</h1>
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex justify-between items-center mt-12 py-6 md:py-12">
+			<div className="flex items-center">
+				<Image
+					className="mr-4 dark:filter dark:invert"
+					src="/logo.svg"
+					alt="Logo"
+					width={60}
+					height={60}
+				/>
+				<h1 className="text-3xl md:text-4xl">cogniforge</h1>
+			</div>
+		</div>
+	);
 }
